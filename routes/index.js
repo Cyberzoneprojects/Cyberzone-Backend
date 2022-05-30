27d9865259cd1fafb3215fb3eafe381fa3bbc3c7
Cyberzoneprojects <cyberzone683@gmail.com>
2022-05-30 23:41:29 +0100

    unit update

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@
   
 const router = require('express').Router()
 const moduleRoute = require('./module.routes')
-const exerciseRoutes = require('./unit.routes')
+const unitRoute = require('./unit.routes')
 
 module.exports = () =>{
     
     router.use('/module', moduleRoute())
-    router.use('/unit', exerciseRoutes())
+    router.use('/unit', unitRoute())
 
 
     return router
