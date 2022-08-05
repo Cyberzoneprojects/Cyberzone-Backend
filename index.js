5ab408033694e97ff017dd6353e19b3c3cee9a30
Cyberzoneprojects <cyberzone683@gmail.com>
2022-08-05 07:52:27 +0100

    initial file uploads

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const routes = require('./routes')
 const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const multer = require('multer')
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -22,6 +23,24 @@ app.use((err, req, res, next)=>{
     res.status(500).json({err: err.message})
 })
 
+///////////////  images upload ///////////////////
+/// Setting up the storage
+var storage = multer.diskStorage({
+    destination: './images',
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
+})
+const upload = multer({storage: storage})
+app.use(express.static(__dirname + "/../build"))
+/// Route for file upload
+app.post("/api/v1/upload", upload.single('myFile'), (req, res,next)=>{
+    res.sendStatus(200);
+})
+//// route for getting files from the server
+app.get('/api/v1/upload')
+app.use('/api/v1/images', express.static('images'))
+/////////////////////////////////////////////////////////////////
 
 app.use(errorHandler);
 
