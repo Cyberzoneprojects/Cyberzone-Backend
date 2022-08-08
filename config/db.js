ba920cae16ffaacdb6a22d123bfe324ab85df2c9
Cyberzoneprojects <cyberzone683@gmail.com>
2022-08-08 23:10:28 +0100

    etrt

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost/cyberzone')
+    const conn = await mongoose.connect('mongodb://localhost:27017/cyberzone')
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.underline)
   } catch (error) {
